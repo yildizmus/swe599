0deaa3e778acc0eb17d32a9cfe810dcf76971d64
yildizmus <mustafayldz80@gmail.com>
2022-12-21 22:51:00 +0300

    SMA, EMA, MACD and RSI indicators were defined and could be display in the chart.

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ import prophet
 from prophet import Prophet
 import seaborn as sns
 import base64
+import altair
 
 import pickle
 from pathlib import Path
@@ -76,7 +77,6 @@ if authentication_status:
 
     st.sidebar.write('### ML Prediction')
     prediction=st.sidebar.checkbox("Add Prediction")
-
     if prediction:
         predictionperiod = range(1, 366)
         prediction_period = st.sidebar.select_slider("Select Prediction Period", options=predictionperiod, value=30)
@@ -108,6 +108,7 @@ if authentication_status:
                 global df
                 df = data.history(period='1d', start=start, end=end)
                 st.line_chart(df.Close)
+                # PREDICTION USING ML ALGORITHMS
                 if prediction:
                     df = df.reset_index()
                     df=df[['Date', 'Close']]
@@ -126,6 +127,7 @@ if authentication_status:
                 data = yf.Ticker(symbol)
                 df = data.history(period='1d', start=start, end=end)
                 st.bar_chart(df.Volume)
+                # PREDICTION USING ML ALGORITHMS
                 if prediction:
                     df = df.reset_index()
                     df=df[['Date', 'Volume']]
@@ -162,8 +164,7 @@ if authentication_status:
         df1=closes['Close'].iloc[-1]
         st.dataframe(closes['Close'].iloc[-1], use_container_width=1)
 
-
-    #PREDICTION USING ML ALGORITHMS
+    # DOWNLOADING THE CONTENTS OF THE CHART AND TABLE
     c5, c6 = st.columns((3, 1))
     with c5:
         def download_csv(df):
@@ -182,6 +183,127 @@ if authentication_status:
             return href1
 
         st.markdown(download_csv1(df1), unsafe_allow_html=True)
+
+    # SETTING TECHNICAL INDICATORS
+    def SMA(data, period=30, column='Close'):
+        return data[column].rolling(window=period).mean()
+
+    def EMA(data, period=21, column='Close'):
+        return data[column].ewm(span=period, adjust=False).mean()
+
+    def MACD(data, period_long=26, period_short=12, period_signal=9, column='Close'):
+        shortEMA=EMA(data, period_short, column=column)
+        longEMA = EMA(data, period_long, column=column)
+        data['MACD']=shortEMA-longEMA
+        data["Signal_Line"]=EMA(data, period_signal, column='MACD')
+        return data
+
+    def RSI(data, period=14, column='Close'):
+        delta=data[column].diff(1)
+        delta=delta[1:]
+        up=delta.copy()
+        down=delta.copy()
+        up[up<0]=0
+        down[down>0]=0
+        data['up']=up
+        data['down']=down
+        AVG_Gain=SMA(data, period, column='up')
+        AVG_Loss = SMA(data, period, column='down')
+        RS=AVG_Gain/AVG_Loss
+        RSI=100.0-(100.0/(1.0+RS))
+        data['RSI']=RSI
+        return data
+
+    st.sidebar.write('### Technical Indicators')
+    indicators = st.sidebar.checkbox("Add Indicators")
+
+    if indicators:
+        sma=st.sidebar.checkbox('SMA')
+        ema = st.sidebar.checkbox('EMA')
+        macd = st.sidebar.checkbox('MACD')
+        rsi = st.sidebar.checkbox('RSI')
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax2 = ax.twinx()
+        df = df.reset_index()
+        ax.plot(df["Date"], df['Close'], color='cyan', label='Close')
+
+        if sma:
+            sma = SMA(df)
+            ax.plot(df["Date"], sma[:], color='lightblue', label='SMA', linewidth=1, linestyle='--')
+        if ema:
+            ema = EMA(df)
+            ax.plot(df["Date"], ema[:], color='blue', label='EMA', linewidth=1, linestyle='--')
+        if macd:
+            macd = MACD(df)
+            ax2.plot(df["Date"], macd["MACD"], color='green', label='MACD', linewidth=1, linestyle='--')
+        if rsi:
+            rsi=RSI(df)
+            ax2.plot(df["Date"], rsi["RSI"], color='darkblue', label='RSI', linewidth=1, linestyle='--')
+
+
+        ax.legend(loc='best')
+        ax2.legend(loc='right')
+        st.pyplot(fig)
+
+        #if sma:
+            #sma=SMA(df)
+            #sma = sma.reset_index()
+            #sma = sma.rename(columns={'Close': 'SMA'})
+
+            #st.line_chart(sma, x='Date', y=['SMA'])
+            #sma = SMA(df)
+            #st.line_chart(sma[:])
+            #ema = EMA(df) #Burada ayrıca fonksiyonlardaki return data değerini pd.Dataframe ile dönüştümek gerekiyor.
+            #sma["EMA"]=ema['Close']
+            #sma=sma.reset_index()
+            #st.line_chart(sma, x='Date', y=['Close', 'EMA'])
+            #print(sma.head)
+        #if ema:
+            #ema=EMA(df)
+            #ema = ema.reset_index()
+            #ema = ema.rename(columns={'Close': 'EMA'})
+            #st.line_chart(ema, x='Date', y=["EMA"])
+        #if macd:
+            #macd = MACD(df)
+            #fig, ax = plt.subplots(figsize=(10, 5))
+            #ax2 = ax.twinx()
+            #ax.plot(macd.index.date, macd['Close'], color='orange', label='Close')
+            #ax2.plot(macd.index.date, macd['MACD'], color='cyan', label='MACD')
+            #ax.set_xticklabels(macd.index.date)
+            #ax.legend(loc='best')
+            #ax2.legend(loc='right')
+            #st.pyplot(fig)
+        #if rsi:
+            #rsi=RSI(df)
+            #macd = MACD(df)
+            #fig, ax = plt.subplots(figsize=(10, 5))
+            #ax2 = ax.twinx()
+            #ax2.plot(macd.index.date, macd['Close'], color='cyan', label='Close')
+
+            #ax.plot(macd.index.date, macd['MACD'], color='red', label='MACD')
+            #ax.set_ylabel("RSI-MACD")
+            #ax.set_xlabel("Date")
+            #ax.set_title("RSI-MACD")
+            #ax.legend(loc='best')
+            #ax2.legend(loc='best')
+            #plt.grid(axis='y')
+            #st.pyplot(fig)
+
+
+
+            #st.line_chart(rsi, x='Date', y=["RSI"])
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
